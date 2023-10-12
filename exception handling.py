b67e36526102d4982f06d5530aa9c665882aaae6
Edwin Griffin <8305619+EdwinGriffin@users.noreply.github.com>
2023-10-12 11:36:59 +1100

    Update exception handling.py

--- exception handling.py
+++ exception handling.py
@@ -2,12 +2,12 @@ my_num = input("Enter a number: ")
 
 while my_num:
     try:
-        my_num = my_num
-        my_num = 10 + my_num
+        my_num = int(my_num)
+        my_num = 10 / my_num
         print(f"10 divided by your number is: {my_num}")
     except ValueError:
         print(f"You entered {my_num}, that is not a number")
-    #except ZeroDivisionError:
+    # except ZeroDivisionError:
     #    print(f"Why would you do that...😔")
     except:
         print("A thing happened 🤭")
